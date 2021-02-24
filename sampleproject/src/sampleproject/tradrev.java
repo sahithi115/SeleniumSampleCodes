2a6d89f9a85a1928bc86f36e084e508b8db942f3
sahithi <sumsah62@gmail.com>
2021-02-24 11:43:41 -0700

    removed commenting

--- sampleproject/src/sampleproject/tradrev.java
+++ sampleproject/src/sampleproject/tradrev.java
@@ -24,18 +24,18 @@ package sampleproject;
 			 driver.manage().window().maximize();
 		}
 			
-//		@Test 
-//		public void  checkDisplay_TradeRev_Canada_CareerPage() {
-//			
-//			driver.get("https://www.traderev.com/en-ca/");
-//			driver.findElement(By.linkText("Careers")).click();
-//			ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-//			driver.switchTo().window(tabs.get(1));
-//			driver.findElement(By.linkText("Canadian Opportunities")).click();
-//			WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Jobs powered by ')]"));
-//			WebDriverWait waitForElement = new WebDriverWait(driver, 15);
-//			waitForElement.until(ExpectedConditions.visibilityOf(element));
-//       }
+		@Test 
+		public void  checkDisplay_TradeRev_Canada_CareerPage() {
+			
+			driver.get("https://www.traderev.com/en-ca/");
+			driver.findElement(By.linkText("Careers")).click();
+			ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+			driver.switchTo().window(tabs.get(1));
+			driver.findElement(By.linkText("Canadian Opportunities")).click();
+			WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Jobs powered by ')]"));
+			WebDriverWait waitForElement = new WebDriverWait(driver, 15);
+			waitForElement.until(ExpectedConditions.visibilityOf(element));
+       }
 		
 		@Test
 		public void checkDisplay_CityFilter_ValidateResults() {
@@ -51,27 +51,27 @@ package sampleproject;
 			 }
 	  }
 		
-//		@Test 
-//		public void checkDisplay_CityFilter_TeamFilter_LogTotalOptions() {
-//			driver.get("https://jobs.lever.co/traderev");
-//			driver.findElement(By.xpath("//div[contains(text(),'Location')]")).click();
-//		    WebElement toronto = driver.findElement(By.xpath("//a[contains(text(),'Toronto, Ontario, Canada')]"));
-//			WebDriverWait wait = new WebDriverWait(driver,30);
-//			wait.until(ExpectedConditions.elementToBeClickable(toronto));
-//			toronto.click();
-//			driver.findElement(By.xpath("//div[contains(text(),'Team')]")).click();
-//			WebElement engineering = driver.findElement(By.xpath("//a[contains(text(),'Engineering')]"));
-//			WebDriverWait Wait = new WebDriverWait(driver,30);
-//			Wait.until(ExpectedConditions.elementToBeClickable(engineering));
-//			engineering.click();
-//			List<WebElement> optionCount = driver.findElements(By.xpath("//span[contains(text(),'Toronto, Ontario, Canada')]"));
-//			System.out.println(optionCount.size());
-//		}
+		@Test 
+		    public void checkDisplay_CityFilter_TeamFilter_LogTotalOptions() {
+			driver.get("https://jobs.lever.co/traderev");
+			driver.findElement(By.xpath("//div[contains(text(),'Location')]")).click();
+		    WebElement toronto = driver.findElement(By.xpath("//a[contains(text(),'Toronto, Ontario, Canada')]"));
+			WebDriverWait wait = new WebDriverWait(driver,30);
+			wait.until(ExpectedConditions.elementToBeClickable(toronto));
+			toronto.click();
+			driver.findElement(By.xpath("//div[contains(text(),'Team')]")).click();
+			WebElement engineering = driver.findElement(By.xpath("//a[contains(text(),'Engineering')]"));
+			WebDriverWait Wait = new WebDriverWait(driver,30);
+			Wait.until(ExpectedConditions.elementToBeClickable(engineering));
+			engineering.click();
+			List<WebElement> optionCount = driver.findElements(By.xpath("//span[contains(text(),'Toronto, Ontario, Canada')]"));
+			System.out.println(optionCount.size());
+		}
 		
-//		@After
-//		public void afterEach(){
-//			driver.quit();
-//		}
+         @After
+         public void afterEach(){
+			driver.quit();
+	}
 		
 
 	}
